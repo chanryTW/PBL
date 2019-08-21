d37f64d0fa6bcaa8159b031a03c720c281c8b0dc
Chanry <chanrytw@gmail.com>
2019-08-22 01:10:33 +0800

    1

--- js/controllers.js
+++ js/controllers.js
@@ -664,9 +664,11 @@ function ($scope, $stateParams, $sce, $state) {
                     if (change.type === "added") {
                         console.log("新增: ", change.doc.data());
                         // 判斷是否 關閉3 完成1 過期2
-                        var lock = 1;
+                        var lock = 0;
                         if (change.doc.data().lock == true){
                             lock = 3;
+                        } else if (change.doc.data().finished.indexOf(StuID)!=-1){
+                            lock = 1;
                         } else if (change.doc.data().TimeOut.toDate() < new Date()){
                             lock = 2;
                         }
